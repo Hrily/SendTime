6b78f2ce3f161f404be905a9a0cf6f092de564ee
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2016-08-28 20:33:24 +0530

    Modified styling

--- SendTime.py
+++ SendTime.py
@@ -1,39 +1,37 @@
 ## Hrishi Hiraskar
 ## 25 August 2016
-import gevent
-import gevent.monkey
-import time
-from gevent.pywsgi import WSGIServer
-gevent.monkey.patch_all()
 
+import gevent
+import time
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
 from flask import Flask, request, Response, render_template
+
+monkey.patch_all()
 
 app = Flask(__name__)
 
 def event_stream():
-    for count in range(32):
-		#start sending timestamp
-		if count < 31:
-		    yield 'event: timestamp\ndata: %s\n\n' % int(round(time.time() * 1000))
-			#wait for 2 secs
-		    gevent.sleep(2)
-		else:
-			#stop sending data
+	for count in range(31):
+		# Start sending timestamp
+		yield 'event: timestamp\ndata: %s\n\n' % int(round(time.time() * 1000))
+		# Wait for 2 secs
+		gevent.sleep(2)
+		if count==30:
+			# Stop sending data
 			yield 'event: DONE\ndata: NA\n\n'
 		
 @app.route('/SendTime')
 def sse_request():
-	#set response method to event-stream
-    return Response(
-            event_stream(),
-            mimetype='text/event-stream')
+	# Set response method to event-stream
+	return Response(event_stream(), mimetype='text/event-stream')
 
 @app.route('/')
 def page():
-    return render_template('SendTime.html')
+	return render_template('index.html')
 
 if __name__ == '__main__':
-	#set server address 127.0.0.1:8080/
-    http_server = WSGIServer(('127.0.0.1', 8001), app)
-    http_server.serve_forever()
+	# Set server address 127.0.0.1:8080/
+	http_server = WSGIServer(('127.0.0.1', 8001), app)
+	http_server.serve_forever()
 
